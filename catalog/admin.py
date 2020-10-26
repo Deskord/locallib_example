9c12861fd78493127a93b6fdc818daa697d97863
neno <goldsok3@gmail.com>
2020-10-26 22:53:07 +0300

    loc_lib v1

---
+++ catalog/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+# Register your models here.
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdnim(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+admin.site.register(Author, AuthorAdnim)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'display_genre']
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'status', 'borrower', 'due_back']
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
